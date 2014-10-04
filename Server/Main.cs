cd6d2c822270d1a72104f5f2cbfe6d390f59d077
Alexandre Oliveira <rockytvbr@gmail.com>
2014-10-03 22:32:27 -0300

    Saved.

--- Server/Main.cs
+++ Server/Main.cs
@@ -98,7 +98,7 @@ namespace DarkMultiPlayerServer
                     DarkLog.Normal("Done!");
 
                     StartHTTPServer();
-                    StartRCONServer();
+                    RCON.StartRCONServer();
                     DarkLog.Normal("Done!");
                     DMPPluginHandler.FireOnServerStart();
                     while (serverRunning)
@@ -238,7 +238,7 @@ namespace DarkMultiPlayerServer
             serverStarting = false;
             serverRunning = false;
             StopHTTPServer();
-            StopRCONServer();
+            RCON.StopRCONServer();
         }
         //Restart
         private static void Restart(string commandArgs)
@@ -257,7 +257,7 @@ namespace DarkMultiPlayerServer
             serverStarting = false;
             serverRunning = false;
             ForceStopHTTPServer();
-            ForceStopRCONServer();
+            RCON.ForceStopRCONServer();
         }
         //Gracefully shut down
         private static void CatchExit(object sender, ConsoleCancelEventArgs args)
@@ -388,92 +388,6 @@ namespace DarkMultiPlayerServer
                     DarkLog.Error("Exception while listening to HTTP server!, Exception:\n" + e);
                     Thread.Sleep(1000);
                     httpListener.BeginGetContext(asyncHTTPCallback, httpListener);
-                }
-            }
-        }
-
-        private static void StartRCONServer()
-        {
-            string OS = Environment.OSVersion.Platform.ToString();
-            byte[] bytes = new byte[256];
-            if (Settings.settingsStore.rconPort > 0)
-            {
-                DarkLog.Normal("Starting RCON server...");
-                try
-                {
-                    rconListener = new TcpListener(IPAddress.Parse(Settings.settingsStore.address), Settings.settingsStore.rconPort);
-                    rconListener.Start();
-                    rconListener.BeginAcceptTcpClient(asyncRCONCallback, rconListener);
-
-                    
-                }
-                catch (Exception e)
-                {
-                    DarkLog.Error("Error while starting RCON Server!, Exception: " + e);
-                }
-            }
-        }
-
-        private static void asyncRCONCallback(IAsyncResult result)
-        {
-            try
-            {
-                TcpListener listener = (TcpListener)result.AsyncState;
-                TcpClient client = listener.EndAcceptTcpClient(result);
-
-                byte[] bytes = new byte[256];
-                string data = null;
-
-                NetworkStream stream = client.GetStream();
-                int i;
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    data = Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("[RCON] Received from " + client.Client.RemoteEndPoint + ": " + data);
-
-                    data = data.ToUpper();
-
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("[RCON] Sent '" + data + "' to " + client.Client.RemoteEndPoint);
-                }
-
-                client.Close();
-
-                Console.WriteLine("Client connected and disconnected.");
-            }
-            catch (Exception e)
-            {
-                DarkLog.Error("Error in RCON Callback!, Exception: " + e);
-            }
-        }
-
-        private static void StopRCONServer()
-        {
-            if (Settings.settingsStore.rconPort > 0)
-            {
-                DarkLog.Normal("Stopping RCON server...");
-                rconListener.Stop();
-            }
-        }
-
-        private static void ForceStopRCONServer()
-        {
-            if (Settings.settingsStore.rconPort > 0)
-            {
-                DarkLog.Normal("Force stopping RCON server...");
-                if (rconListener != null)
-                {
-                    try
-                    {
-                        rconListener.Stop();
-                    }
-                    catch (Exception e)
-                    {
-                        DarkLog.Fatal("Error trying to shutdown RCON server: " + e);
-                        throw;
-                    }
                 }
             }
         }
